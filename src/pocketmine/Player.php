b7730cb1ab54fd0fc08125d4f61ef0c3e9d62f8d
Florian <38558911+iTzMeFlorian@users.noreply.github.com>
2020-08-26 21:36:08 +0200

    You can fastbridge now :v

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2484,16 +2484,6 @@ class Player extends Human implements CommandSender, ChunkLoader, IPlayer{
 				switch($type){
 					case InventoryTransactionPacket::USE_ITEM_ACTION_CLICK_BLOCK:
 						//TODO: start hack for client spam bug
-						$spamBug = ($this->lastRightClickPos !== null and
-							microtime(true) - $this->lastRightClickTime < 0.1 and //100ms
-							$this->lastRightClickPos->distanceSquared($packet->trData->clickPos) < 0.00001 //signature spam bug has 0 distance, but allow some error
-						);
-						//get rid of continued spam if the player clicks and holds right-click
-						$this->lastRightClickPos = clone $packet->trData->clickPos;
-						$this->lastRightClickTime = microtime(true);
-						if($spamBug){
-							return true;
-						}
 						//TODO: end hack for client spam bug
 
 						$this->setUsingItem(false);
